3bd99b78eb1dcdc47541db4233e65cec5736009a
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-10-09 22:50:47 +0200

    Add the logic to the app

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+    btn.addEventListener("click",(e) => {
+      const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+    });
+});
+
