a38f03e64cdd77256ef44437d9d8946a023f34f6
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-10-10 10:26:33 +0200

    Improve the logic of the app

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 //using selectors inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+// console.log(question);
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+  btn.addEventListener("click", () => {
+
+    questions.forEach((item) => {
+      // console.log(item);
+      if(item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+
+    question.classList.toggle("show-text");
+  })
+});
+
+
+
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+// const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach((btn) => {
-    btn.addEventListener("click",(e) => {
-      const question = e.currentTarget.parentElement.parentElement;
-        question.classList.toggle("show-text");
-    });
-});
+// btns.forEach((btn) => {
+//     btn.addEventListener("click",(e) => {
+//       const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
+//     });
+// });
 
